591b06e1b89e40c81556dc97371e8e29eeea7bb6
Ludovico Pavesi <ludovico.pavesi@gmail.com>
2019-04-29 23:17:29 +0200

    Pointerkeys stuff, can't tell if it works or not, closes #15 because nobody cares

--- weeedebian_files/martello.sh
+++ weeedebian_files/martello.sh
@@ -64,6 +64,10 @@ if [[ $ans == "y" ]]; then
     sudo -H -u weee cp /weeedebian_files/Tarallo.desktop /home/weee/Desktop
     sudo -H -u weee chmod +x /home/weee/Desktop/Tarallo.desktop
 
+    echo === Pointerkeys thing ===
+    sudo -H u weee mkdir -p /home/weee/.config/autostart
+    sudo -H u weee cp /weeedebian_files/Pointerkeys.desktop /home/weee/.config/autostart/Pointerkeys.desktop
+
     echo === Autologin stuff ===
     sudo -H -u root cp /weeedebian_files/lightdm.conf /etc/lightdm/lightdm.conf
     sudo -H -u root mkdir -p /etc/systemd/system/getty@.service.d
