a48e680f06a96bd3832c3a8b90a86384ed6c9406
Ludovico Pavesi <ludovico.pavesi@gmail.com>
2022-02-26 01:28:36 +0100

    Fix many things
    The old script expected a live system, not an empty bootstrapped one

--- weeedebian/martello.sh
+++ weeedebian/martello.sh
@@ -4,9 +4,18 @@
 
 echo "Martello is starting!"
 
-echo "[SeatDefaults]" >> /usr/share/lightdm/lightdm.conf.d/01_debian.conf
-echo "autologin-user=<nome user>" >> /usr/share/lightdm/lightdm.conf.d/01_debian.conf
-echo "autologin-user-timeout=0"  >> /usr/share/lightdm/lightdm.conf.d/01_debian.conf
+# this has to be done before sudo
+echo "=== Set hostname ==="
+export HOSTNAME $MISO_HOSTNAME
+echo "$MISO_HOSTNAME" > /etc/hostname
+cat << EOF > /etc/hosts
+127.0.0.1       localhost
+::1             localhost ip6-localhost ip6-loopback
+ff02::1         ip6-allnodes
+ff02::2         ip6-allrouters
+127.0.0.1       $MISO_HOSTNAME
+::1             $MISO_HOSTNAME
+EOF
 
 echo "=== Software installation ==="
 # Remove useless packages, courtesy of "wajig large". Cool command.
@@ -14,10 +23,10 @@ echo "=== Software installation ==="
 #/bin/bash -c 'DEBIAN_FRONTEND=noninteractive apt-getpurge --auto-remove -y libreoffice libreoffice-core libreoffice-common ispell* gimp gimp-* aspell* hunspell* mythes* *sunpinyin* wpolish wnorwegian tegaki* task-thai task-thai-desktop xfonts-thai xiterm* task-khmer task-khmer-desktop fonts-khmeros khmerconverter'
 # Upgrade and install useful packages
 export DEBIAN_FRONTEND=noninteractive
-apt-get update -y
-apt-get upgrade -y
+apt-get -qq update -y
+apt-get -qq upgrade -y
 # libxkbcommon-x11-0 may be not needed (see Add library to installation if needed #28)
-apt-get install -y\
+apt-get -qq install -y\
     apt-transport-https \
     ca-certificates \
     cifs-utils \
@@ -34,13 +43,13 @@ apt-get install -y\
     iputils-tracepath \
     libglu1-mesa-dev \
     libx11-xcb-dev \
-    '^libxcb.*-dev' \
     libxi-dev \
     libxkbcommon-dev \
     libxkbcommon-x11-0 \
     libxkbcommon-x11-dev \
     libxrender-dev \
     lightdm \
+    locales \
     lshw \
     mesa-utils \
     nano \
@@ -52,7 +61,6 @@ apt-get install -y\
     pciutils \
     rsync \
     smartmontools \
-    sudo \
     sudo \
     traceroute \
     wget \
@@ -66,6 +74,7 @@ apt-get install -y\
     xserver-xorg \
     xserver-xorg-core \
     zsh
+apt-get -qq install -y
 update-ca-certificates
 
 echo "=== User configuration ==="
@@ -82,11 +91,18 @@ fi
 # So...
 sed -i "s#root:.*#root:$ROOTPASSWD:18214:0:99999:7:::#" /etc/shadow
 sed -i "s#$MISO_USERNAME:.*#$MISO_USERNAME:$MISO_USERPASSWD:18214:0:99999:7:::#" /etc/shadow
+
+echo "=== Sudo configuration ==="
+cp ./$MISO_USERNAME /etc/sudoers.d/$MISO_USERNAME
+
+echo "=== Shell and home configuration ==="
+sudo -u $MISO_USERNAME mkdir -p /home/$MISO_USERNAME/Desktop
+sudo -u $MISO_USERNAME ln -s ./Desktop /home/$MISO_USERNAME/Scrivania
 chsh -s /bin/zsh root
 # chsh -s /bin/zsh weee
-runuser -l $MISO_USERNAME -c "curl -L -o /home/$MISO_USERNAME/.zshrc https://git.grml.org/f/grml-etc-core/etc/zsh/zshrc"
+sudo -u $MISO_USERNAME curl -L -o /home/$MISO_USERNAME/.zshrc https://git.grml.org/f/grml-etc-core/etc/zsh/zshrc
 cp /home/$MISO_USERNAME/.zshrc /root/.zshrc
-runuser -l $MISO_USERNAME -c "rm /home/$MISO_USERNAME/.bash_history  >/dev/null 2>/dev/null"
+sudo -u $MISO_USERNAME rm /home/$MISO_USERNAME/.bash_history  >/dev/null 2>/dev/null
 rm /root/.bash_history >/dev/null 2>/dev/null
 
 echo "=== Keymap configuration ==="
@@ -118,9 +134,6 @@ if [[ -z `grep at24 /etc/modules-load.d/eeprom.conf` ]]; then
     printf "at24\n" > /etc/modules-load.d/eeprom.conf
 fi
 
-echo "=== Sudo configuration ==="
-cp ./weee /etc/sudoers.d/weee
-
 echo "=== DNS configuration ==="
 cp ./resolv.conf /etc/resolv.conf
 cp ./resolved.conf /etc/systemd/resolved.conf
@@ -133,10 +146,11 @@ ln -s /usr/share/zoneinfo/Europe/Rome /etc/localtime
 
 echo "=== Top configuration ==="
 cp ./toprc /root/.toprc
-runuser -l $MISO_USERNAME -c "cp ./toprc /home/$MISO_USERNAME/.toprc"
+sudo -u $MISO_USERNAME cp ./toprc /home/$MISO_USERNAME/.toprc
 
 echo "=== Prepare peracotta ==="
-/bin/bash -c 'apt-get install -y python3-pip'
+# TODO: ln libxcb
+apt-get -qq install -y python3-pip
 # PyQt > 5.14.0 requires an EXTREMELY RECENT version of pip,
 # on the most bleeding of all bleeding edges
 python3 -m pip install --quiet --upgrade pip
@@ -145,46 +159,46 @@ cp ./peracotta_update /etc/cron.d/peracotta_update
 
 if [[ -d "/home/$MISO_USERNAME/peracotta" ]]; then
   rm -rf "/home/$MISO_USERNAME/peracotta"
-  #runuser -l $MISO_USERNAME -c "git -C /home/$MISO_USERNAME/peracotta pull"
+  #sudo -u $MISO_USERNAME git -C /home/$MISO_USERNAME/peracotta pull
 fi
 #else
-runuser -l $MISO_USERNAME -c "mkdir -p /home/$MISO_USERNAME/peracotta"
-runuser -l $MISO_USERNAME -c "git clone https://github.com/WEEE-Open/peracotta.git /home/$MISO_USERNAME/peracotta"
+sudo -u $MISO_USERNAME mkdir -p /home/$MISO_USERNAME/peracotta
+sudo -u $MISO_USERNAME git clone https://github.com/WEEE-Open/peracotta.git /home/$MISO_USERNAME/peracotta
 #fi
 
-#runuser -l $MISO_USERNAME -c "sh -c 'cd /home/$MISO_USERNAME/peracotta && python3 polkit.py'"
-runuser -l $MISO_USERNAME -c "cp ./features.json /home/$MISO_USERNAME/peracotta/features.json"
+#sudo -u $MISO_USERNAME sh -c 'cd /home/$MISO_USERNAME/peracotta && python3 polkit.py'
+sudo -u $MISO_USERNAME cp ./features.json /home/$MISO_USERNAME/peracotta/features.json
 
-if [[ "x$(dpkg --print-architecture)" == "xi386" ]]; then
+if [[ "$MISO_ARCH" == "i386" ]]; then
   echo ""===== Begin incredible workaround for PyQt on 32 bit =====""
-  /bin/bash -c 'apt-get install -y python3-pyqt5'
-  runuser -l $MISO_USERNAME -c "/bin/bash -c \"grep -vi "pyqt" /home/$MISO_USERNAME/peracotta/requirements.txt > /home/$MISO_USERNAME/peracotta/requirements32.txt\""
+  apt-get -qq install -y python3-pyqt5
+  sudo -u $MISO_USERNAME /bin/bash -c "grep -vi pyqt /home/$MISO_USERNAME/peracotta/requirements.txt > /home/$MISO_USERNAME/peracotta/requirements32.txt"
   pip3 --quiet install -r /home/$MISO_USERNAME/peracotta/requirements32.txt
   rm -f /home/$MISO_USERNAME/peracotta/requirements32.txt
   echo ""===== End incredible workaround for PyQt on 32 bit =====""
 else
-  /bin/bash -c 'apt-get autoremove -y python3-pyqt5'
+  apt-get -qq autoremove -y python3-pyqt5
   pip3 --quiet install -r /home/$MISO_USERNAME/peracotta/requirements.txt
 fi
 
-PERACOTTA_GENERATE_FILES=$(runuser -l $MISO_USERNAME -c "find /home/$MISO_USERNAME/peracotta -name generate_files* -print -quit")
-PERACOTTA_MAIN=$(runuser -l $MISO_USERNAME -c "find /home/$MISO_USERNAME/peracotta -name peracruda -print -quit")
-PERACOTTA_MAIN_WITH_GUI=$(runuser -l $MISO_USERNAME -c "find /home/$MISO_USERNAME/peracotta -name peracotta -print -quit")
+PERACOTTA_GENERATE_FILES=$(sudo -u $MISO_USERNAME find /home/$MISO_USERNAME/peracotta -name generate_files.sh -print -quit)
+PERACOTTA_CLI=/home/$MISO_USERNAME/peracotta/peracruda
+PERACOTTA_GUI=/home/$MISO_USERNAME/peracotta/peracotta
 
 if [[ -f "$PERACOTTA_GENERATE_FILES" ]]; then
-  runuser -l $MISO_USERNAME -c 'chmod +x "$PERACOTTA_GENERATE_FILES"'
-  ln -s "$PERACOTTA_GENERATE_FILES" /usr/bin/generate_files
+  sudo -u $MISO_USERNAME chmod +x "$PERACOTTA_GENERATE_FILES"
+  ln -sf "$PERACOTTA_GENERATE_FILES" /usr/bin/generate_files
 fi
-if [[ -f "$PERACOTTA_MAIN" ]]; then
-  runuser -l $MISO_USERNAME -c 'chmod +x "$PERACOTTA_MAIN"'
+if [[ -f "$PERACOTTA_CLI" ]]; then
+  sudo -u $MISO_USERNAME chmod +x "$PERACOTTA_CLI"
 fi
-if [[ -f "$PERACOTTA_MAIN_WITH_GUI" ]]; then
-  runuser -l $MISO_USERNAME -c 'chmod +x "$PERACOTTA_MAIN_WITH_GUI"'
+if [[ -f "$PERACOTTA_GUI" ]]; then
+  sudo -u $MISO_USERNAME chmod +x "$PERACOTTA_GUI"
 fi
 
 echo "=== Add env to peracotta ==="
 if [[ -f "./env.txt" ]]; then
-  runuser -l $MISO_USERNAME -c "cp ./env.txt /home/$MISO_USERNAME/peracotta/.env"
+  sudo -u $MISO_USERNAME cp ./env.txt /home/$MISO_USERNAME/peracotta/.env
 else
   echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
   echo "@                                                          @"
@@ -203,43 +217,41 @@ fi
 echo "=== s.sh ==="
 cp ./s.sh /usr/sbin/s.sh
 chmod +x /usr/sbin/s.sh
-runuser -l $MISO_USERNAME -c "cp ./ssh.desktop /home/$MISO_USERNAME/Desktop"
-runuser -l $MISO_USERNAME -c "chmod +x /home/$MISO_USERNAME/Desktop/ssh.desktop"
+sudo -u $MISO_USERNAME cp ./ssh.desktop /home/$MISO_USERNAME/Desktop
+sudo -u $MISO_USERNAME chmod +x /home/$MISO_USERNAME/Desktop/ssh.desktop
 
 echo "=== XFCE configuration ==="
-runuser -l $MISO_USERNAME -c "mkdir -p /home/$MISO_USERNAME/.config/xfce4"
+sudo -u $MISO_USERNAME mkdir -p /home/$MISO_USERNAME/.config/xfce4
 rsync -a --force ./xfce4 /home/$MISO_USERNAME/.config
 chown weee: -R /home/$MISO_USERNAME/.config
-runuser -l $MISO_USERNAME -c "mkdir /home/$MISO_USERNAME/.config/xfce4/desktop /home/$MISO_USERNAME/.config/xfce4/terminal"
+sudo -u $MISO_USERNAME mkdir /home/$MISO_USERNAME/.config/xfce4/desktop /home/$MISO_USERNAME/.config/xfce4/terminal
 
 echo "=== Desktop shortcuts ==="
 if [[ -d "/home/$MISO_USERNAME/limone" ]]; then
-  runuser -l $MISO_USERNAME -c "git -C /home/$MISO_USERNAME/limone pull"
+  sudo -u $MISO_USERNAME git -C /home/$MISO_USERNAME/limone pull
 else
-  runuser -l $MISO_USERNAME -c "mkdir -p /home/$MISO_USERNAME/limone"
-  runuser -l $MISO_USERNAME -c "git clone https://github.com/WEEE-Open/limone.git /home/$MISO_USERNAME/limone"
+  sudo -u $MISO_USERNAME mkdir -p /home/$MISO_USERNAME/limone
+  sudo -u $MISO_USERNAME git clone https://github.com/WEEE-Open/limone.git /home/$MISO_USERNAME/limone
 fi
 
-runuser -l $MISO_USERNAME -c "mkdir -p /home/$MISO_USERNAME/Desktop"
-
-for desktop_file in $(runuser -l $MISO_USERNAME -c "find /home/$MISO_USERNAME/limone -name \"*.desktop\" -type f -printf \"%f \""); do
-  runuser -l $MISO_USERNAME -c "cp \"/home/$MISO_USERNAME/limone/\$desktop_file\" \"/home/$MISO_USERNAME/Desktop/\$desktop_file\""
-  runuser -l $MISO_USERNAME -c "chmod +x \"/home/$MISO_USERNAME/Desktop/$desktop_file\""
+for desktop_file in $(sudo -u $MISO_USERNAME find /home/$MISO_USERNAME/limone -name "*.desktop" -type f -printf "%f "); do
+  sudo -u $MISO_USERNAME cp "/home/$MISO_USERNAME/limone/$desktop_file" "/home/$MISO_USERNAME/Desktop/$desktop_file"
+  sudo -u $MISO_USERNAME chmod +x "/home/$MISO_USERNAME/Desktop/$desktop_file"
   sed -ri -e "s#Icon=(.*/)*([a-zA-Z0-9\-\.]+)#Icon=/home/$MISO_USERNAME/limone/\2#" "/home/$MISO_USERNAME/Desktop/$desktop_file"
 done
 
-runuser -l $MISO_USERNAME -c "cp ./Peracotta.desktop /home/$MISO_USERNAME/Desktop"
-runuser -l $MISO_USERNAME -c "cp ./peracotta.png /home/$MISO_USERNAME/.config/peracotta.png"
-runuser -l $MISO_USERNAME -c "chmod +x /home/$MISO_USERNAME/Desktop/Peracotta.desktop"
+sudo -u $MISO_USERNAME cp ./Peracotta.desktop /home/$MISO_USERNAME/Desktop
+sudo -u $MISO_USERNAME cp ./peracotta.png /home/$MISO_USERNAME/.config/peracotta.png
+sudo -u $MISO_USERNAME chmod +x /home/$MISO_USERNAME/Desktop/Peracotta.desktop
 
-runuser -l $MISO_USERNAME -c "cp ./PeracottaGUI.desktop /home/$MISO_USERNAME/Desktop"
-runuser -l $MISO_USERNAME -c "cp ./peracotta_gui.png /home/$MISO_USERNAME/.config/peracotta_gui.png"
-runuser -l $MISO_USERNAME -c "chmod +x /home/$MISO_USERNAME/Desktop/PeracottaGUI.desktop"
+sudo -u $MISO_USERNAME cp ./PeracottaGUI.desktop /home/$MISO_USERNAME/Desktop
+sudo -u $MISO_USERNAME cp ./peracotta_gui.png /home/$MISO_USERNAME/.config/peracotta_gui.png
+sudo -u $MISO_USERNAME chmod +x /home/$MISO_USERNAME/Desktop/PeracottaGUI.desktop
 
 echo "=== Pointerkeys thing ==="
-runuser -l $MISO_USERNAME -c "mkdir -p /home/$MISO_USERNAME/.config/autostart"
-runuser -l $MISO_USERNAME -c "cp ./Pointerkeys.desktop /home/$MISO_USERNAME/.config/autostart/Pointerkeys.desktop"
-runuser -l $MISO_USERNAME -c "cp ./pointerkeys.txt /home/$MISO_USERNAME/Desktop"
+sudo -u $MISO_USERNAME mkdir -p /home/$MISO_USERNAME/.config/autostart
+sudo -u $MISO_USERNAME cp ./Pointerkeys.desktop /home/$MISO_USERNAME/.config/autostart/Pointerkeys.desktop
+sudo -u $MISO_USERNAME cp ./pointerkeys.txt /home/$MISO_USERNAME/Desktop
 
 echo "=== Autologin stuff ==="
 cat << EOF > /etc/lightdm/lightdm.conf
@@ -255,17 +267,14 @@ printf "[Service]\n" > /etc/systemd/system/getty@.service.d/override.conf
 printf "ExecStart=\n" >> /etc/systemd/system/getty@.service.d/override.conf
 printf "ExecStart=-/sbin/agetty --noissue --autologin weee %%I $TERM" >> /etc/systemd/system/getty@.service.d/override.conf
 
-echo "=== Set hostname ==="
-echo "$MISO_HOSTNAME" > /etc/hostname
-
 echo "=== Final cleanup ==="
 # Remove unused packages
-/bin/bash -c 'apt-get autoremove -y'
+apt-get -qq autoremove -y
 # Clean the cache
-/bin/bash -c 'apt-get clean -y'
+apt-get -qq clean -y
 
-  echo "=== Automatic configuration done ==="
-  read -p 'Open a shell in the chroot environment? [y/n] ' ans
-      if [[ $ans == "y" ]]; then
-          runuser -l $MISO_USERNAME -c '/bin/bash'
-      fi
+echo "=== Automatic configuration done ==="
+#  read -p 'Open a shell in the chroot environment? [y/n] ' ans
+#      if [[ $ans == "y" ]]; then
+#          sudo -u $MISO_USERNAME /bin/bash
+#      fi
