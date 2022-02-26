96086f8d35ec270b4b6de077d21d9d226db47571
Ludovico Pavesi <ludovico.pavesi@gmail.com>
2022-02-26 01:28:24 +0100

    Update Debian version and everything else

--- miso.sh
+++ miso.sh
@@ -26,15 +26,6 @@ else
     _SUDO=""
 fi
 
-MISO_CHROOT_SCRIPT=$(readlink -f "$MISO_CHROOT_SCRIPT")
-_MISO_SOURCE_DIR=$(dirname "$MISO_CHROOT_SCRIPT")
-_MISO_SOURCE_SCRIPT=$(basename "$MISO_CHROOT_SCRIPT")
-_MISO_BUILD_NAME=$(basename "$_MISO_SOURCE_DIR")
-MISO_BUILD_DIR=$(readlink -f "$MISO_BUILD_DIR")
-MISO_BUILD_DIR="$MISO_BUILD_DIR/$_MISO_BUILD_NAME"
-
-export MISO_BUILD_DIR
-
 if [[ -z "$MISO_ARCH" ]]; then
     if [[ -z "$3" ]]; then
         echo "Set the architecture as the third parameter or via MISO_ARCH"
@@ -54,8 +45,10 @@ done
 
 while [[ -z "$MISO_ROOTPASSWD" ]]; do
   echo -n "Root password: "
-  read -s MISO_ROOTPASSWD
-  echo
+  # read -s doesn't work from docker
+  stty -echo
+  read MISO_ROOTPASSWD
+  stty echo
 done
 
 while [[ -z "$MISO_USERNAME" ]]; do
@@ -65,8 +58,9 @@ done
 
 while [[ -z $MISO_USERPASSWD ]]; do
   echo -n "$MISO_USERNAME password: "
-  read -s MISO_USERPASSWD
-  echo
+  stty -echo
+  read MISO_USERPASSWD
+  stty echo
 done
 
 _ORANGE='\033[0;33m'
@@ -75,37 +69,29 @@ _BLUE='\033[0;34m'
 _GREEN='\033[0;32m'
 _RESET_COLOR='\033[0m'
 
-# Install build dependencies
-echo -e "${_ORANGE}Installing build dependencies ...${_BLUE}"
-DEBIAN_FRONTEND=noninteractive $MISO_SUDO apt-get install -y \
-    debootstrap \
-    squashfs-tools \
-    xorriso \
-    isolinux \
-    syslinux-efi \
-    grub-pc-bin \
-    grub-efi-amd64-bin \
-    mtools \
-    debian-archive-keyring \
-    isolinux \
-    syslinux
-
 # Build chroot
-if [[ $MISO_ARCH == '32' ]]; then
-    echo -e "${_ORANGE}Building chroot for 32-bit${_RESET_COLOR}${_BLUE}"
-    _DEBOOTRSTRAP_ARCH=i386
+if [[ $MISO_ARCH == '32' || $MISO_ARCH == 'i386' ]]; then
+    echo -e "${_BLUE}Building chroot for 32 bit${_RESET_COLOR}"
+    MISO_ARCH=i386
 else
-    echo -e "${_ORANGE}Building chroot for 64-bit${_RESET_COLOR}${_BLUE}"
-    _DEBOOTRSTRAP_ARCH=amd64
+    echo -e "${_BLUE}Building chroot for 64 bit${_RESET_COLOR}"
+    MISO_ARCH=amd64
 fi
+
+MISO_CHROOT_SCRIPT=$(readlink -f "$MISO_CHROOT_SCRIPT")
+_MISO_SOURCE_DIR=$(dirname "$MISO_CHROOT_SCRIPT")
+_MISO_SOURCE_SCRIPT=$(basename "$MISO_CHROOT_SCRIPT")
+_MISO_BUILD_NAME=$(basename "$_MISO_SOURCE_DIR")
+MISO_BUILD_DIR=$(readlink -f "$MISO_BUILD_DIR")
+MISO_BUILD_DIR="$MISO_BUILD_DIR/$_MISO_BUILD_NAME-$MISO_ARCH"
 
 mkdir -p $MISO_BUILD_DIR
 
 if [[ -z "$MISO_NO_BOOSTRAP" ]]; then
   $MISO_SUDO debootstrap \
-    --arch=$_DEBOOTRSTRAP_ARCH \
+    --arch=$MISO_ARCH \
     --variant=minbase \
-    buster \
+    bullseye \
     $MISO_BUILD_DIR/chroot \
     http://ftp.it.debian.org/debian/
 fi
@@ -113,32 +99,32 @@ fi
 rm -rf "$MISO_BUILD_DIR/chroot/source" 2>/dev/null
 cp -r $_MISO_SOURCE_DIR "$MISO_BUILD_DIR/chroot/source"
 cat << EOF | $MISO_SUDO chroot $MISO_BUILD_DIR/chroot
-DEBIAN_FRONTEND=noninteractive apt-get update
-DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends \
-    linux-image-amd64 \
+DEBIAN_FRONTEND=noninteractive apt-get -qq update
+DEBIAN_FRONTEND=noninteractive apt-get -qq install -y --no-install-recommends \
+    linux-image-$MISO_ARCH \
     live-boot \
     systemd-sysv \
     apt-utils
-DEBIAN_FRONTEND=noninteractive apt-get clean -y
 
-cd /source
 # Subsitution is done outside the chroot
 export MISO_HOSTNAME "$MISO_HOSTNAME"
 export MISO_ROOTPASSWD "$MISO_ROOTPASSWD"
 export MISO_USERNAME "$MISO_USERNAME"
 export MISO_USERPASSWD "$MISO_USERPASSWD"
+export MISO_ARCH "$MISO_ARCH"
+cd /source
 bash ./$_MISO_SOURCE_SCRIPT
 EOF
-rm -rf "$MISO_BUILD_DIR/chroot/source"
+rm -rf "$MISO_BUILD_DIR/chroot/source" 2>/dev/null
 
-# echo "TEST POINT"
-# exit 0
+echo "TEST POINT"
+exit 0
 
 # Create directory tree
 mkdir -p $MISO_BUILD_DIR/{staging/{EFI/boot,boot/grub/x86_64-efi,isolinux,live},tmp}
 
 # Squash filesystem
-echo -e "${_ORANGE}Squashing filesystem ...${_BLUE}"
+echo -e "${_ORANGE}Squashing filesystem ...${_RESET_COLOR}"
 $MISO_SUDO mksquashfs \
     $MISO_BUILD_DIR/chroot \
     $MISO_BUILD_DIR/staging/live/filesystem.squashfs \
@@ -149,7 +135,7 @@ cp $MISO_BUILD_DIR/chroot/boot/vmlinuz-* \
 cp $MISO_BUILD_DIR/chroot/boot/initrd.img-* \
     $MISO_BUILD_DIR/staging/live/initrd
 
-echo -e "${_ORANGE}Building bootloader ...${_BLUE}"
+echo -e "${_ORANGE}Building bootloader ...${_RESET_COLOR}"
 cat <<'EOF' >$MISO_BUILD_DIR/staging/isolinux/isolinux.cfg
 UI vesamenu.c32
 
@@ -168,13 +154,13 @@ MENU COLOR msg07        37;40   #90ffffff #a0000000 std
 MENU COLOR tabmsg       31;40   #30ffffff #00000000 std
 
 LABEL linux
-  MENU LABEL $_MISO_BUILD_NAME $_DEBOOTRSTRAP_ARCH [BIOS/ISOLINUX]
+  MENU LABEL $_MISO_BUILD_NAME $MISO_ARCH [BIOS/ISOLINUX]
   MENU DEFAULT
   KERNEL /live/vmlinuz
   APPEND initrd=/live/initrd boot=live
 
 LABEL linux
-  MENU LABEL $_MISO_BUILD_NAME $_DEBOOTRSTRAP_ARCH [BIOS/ISOLINUX] (nomodeset)
+  MENU LABEL $_MISO_BUILD_NAME $MISO_ARCH [BIOS/ISOLINUX] (nomodeset)
   MENU DEFAULT
   KERNEL /live/vmlinuz
   APPEND initrd=/live/initrd boot=live nomodeset
@@ -188,12 +174,12 @@ set timeout=30
 
 # If X has issues finding screens, experiment with/without nomodeset.
 
-menuentry "$_MISO_BUILD_NAME $_DEBOOTRSTRAP_ARCH [EFI/GRUB]" {
+menuentry "$_MISO_BUILD_NAME $MISO_ARCH [EFI/GRUB]" {
     linux ($root)/live/vmlinuz boot=live
     initrd ($root)/live/initrd
 }
 
-menuentry "$_MISO_BUILD_NAME $_DEBOOTRSTRAP_ARCH [EFI/GRUB] (nomodeset)" {
+menuentry "$_MISO_BUILD_NAME $MISO_ARCH [EFI/GRUB] (nomodeset)" {
     linux ($root)/live/vmlinuz boot=live nomodeset
     initrd ($root)/live/initrd
 }
@@ -225,11 +211,11 @@ mmd -i efiboot.img efi efi/boot && \
 mcopy -vi efiboot.img $MISO_BUILD_DIR/tmp/bootx64.efi ::efi/boot/
 )
 
-echo -e "${_ORANGE}Building final ISO ...${_BLUE}"
+echo -e "${_ORANGE}Building final ISO ...${_RESET_COLOR}"
 xorriso \
     -as mkisofs \
     -iso-level 3 \
-    -o "$MISO_BUILD_DIR/$_MISO_BUILD_NAME-$_DEBOOTRSTRAP_ARCH.iso" \
+    -o "$MISO_BUILD_DIR/$_MISO_BUILD_NAME-$MISO_ARCH.iso" \
     -full-iso9660-filenames \
     -volid "${_MISO_BUILD_NAME^^}" \
     -isohybrid-mbr /usr/lib/ISOLINUX/isohdpfx.bin \
@@ -245,14 +231,3 @@ xorriso \
         -isohybrid-gpt-basdat \
     -append_partition 2 0xef $MISO_BUILD_DIR/staging/EFI/boot/efiboot.img \
     "$MISO_BUILD_DIR/staging"
-
-# TODO: add a way to answer N automatically from the container (use an env var?)
-while true; do
-    echo -e "${_RESET_COLOR}"
-    read -p "Do you want to remove all build dependencies? [y/n]" yn
-    case $yn in
-        [Yy]* ) bash miso_remove_dep.sh; break;;
-        [Nn]* ) echo -e "${_GREEN}Everything done!${_RESET_COLOR}"; exit;;
-        * ) echo "Please answer y or n.";;
-    esac
-done
