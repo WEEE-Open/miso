c42112ac79d4c279d27624e28767a0c3c4d51cc0
Francesco Basile <59255877+parmigggiana@users.noreply.github.com>
2024-03-11 15:46:39 +0100

    explicitly set $CXX

--- weeedebian/martello.sh
+++ weeedebian/martello.sh
@@ -235,6 +235,9 @@ sudo -u $MISO_USERNAME cp ./toprc /home/$MISO_USERNAME/.toprc
 echo "=== Prepare peracotta ==="
 if [ "$HOSTTYPE" = "i686" ]; then
 echo "=== Compile Qt from source ==="
+# cmake complains
+export CXX=/usr/bin/g++
+
 # Both Qt5 and Qt6 don't have precomiled binaries for 32bit architectures on modern versions
 # https://wiki.qt.io/Building_Qt_6_from_Git
 apt-get install -y cmake ninja-build libfontconfig1-dev libgl-dev libegl-dev libfontconfig1-dev libfreetype6-dev libx11-dev libx11-xcb-dev libxext-dev libxfixes-dev libxi-dev libxrender-dev libxcb1-dev libxcb-cursor-dev libxcb-glx0-dev libxcb-keysyms1-dev libxcb-image0-dev libxcb-shm0-dev libxcb-icccm4-dev libxcb-sync-dev libxcb-xfixes0-dev libxcb-shape0-dev libxcb-randr0-dev libxcb-render-util0-dev libxcb-util-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev
